cc01c963691c67d0086bf12dba975a9b673fb1a1
WoLpH <Rick@Fawo.nl>
2013-08-23 19:01:27 +0200

    added samples

---
+++ tests/samples/generators.py
@@ -0,0 +1,13 @@
+def spam():
+    yield 'eggs'
+
+spam = ('egg %d' % egg for egg in range(5))
+
+
+def spam(eggs=123, a={'a': 'b', 'c': 3, 4: 'd', 'e': dict(a=1, b=3)}):
+    for a in range(5) + range(5):
+        yield (a, )
+        b = (yield)
+        assert b
+        return
+
